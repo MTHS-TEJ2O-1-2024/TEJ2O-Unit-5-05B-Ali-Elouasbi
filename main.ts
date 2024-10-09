52c357af698ea106fad92e4772ee817af1b00861
Ali-Elouasbi <ali.elouasbi@mths.ca>
2024-10-09 19:24:49 -0400

    5-05

--- main.ts
+++ main.ts
@@ -1,8 +1,32 @@
 /* Copyright (c) 2020 MTHS All rights reserved
  *
- * Created by: Mr. Coxall
- * Created on: Sep 2020
- * This program ...
+ * Created by: Ali Elouasbi
+ * Created on: Oct 2024
+ * This program uses the RobotBit's Neopixels
 */
 
-basic.showString('Hello, World!')
+// variables
+let neopixelStrip: neopixel.Strip = null
+
+basic.forever(function() {
+    // cleanup
+    basic.clearScreen()
+    neopixelStrip = neopixel.create(DigitalPin.P16, 3, NeoPixelMode.RGB)
+    neopixelStrip.setPixelColor(2, neopixel.colors(NeoPixelColors.Green))
+    neopixelStrip.setPixelColor(1, neopixel.colors(NeoPixelColors.Black))
+    neopixelStrip.setPixelColor(0, neopixel.colors(NeoPixelColors.Black))
+    neopixelStrip.show()
+    basic.pause(2000)
+    neopixelStrip = neopixel.create(DigitalPin.P16, 3, NeoPixelMode.RGB)
+    neopixelStrip.setPixelColor(2, neopixel.colors(NeoPixelColors.Black))
+    neopixelStrip.setPixelColor(1, neopixel.colors(NeoPixelColors.Yellow))
+    neopixelStrip.setPixelColor(0, neopixel.colors(NeoPixelColors.Black))
+    neopixelStrip.show()
+    basic.pause(1000)
+    neopixelStrip = neopixel.create(DigitalPin.P16, 3, NeoPixelMode.RGB)
+    neopixelStrip.setPixelColor(2, neopixel.colors(NeoPixelColors.Black))
+    neopixelStrip.setPixelColor(1, neopixel.colors(NeoPixelColors.Black))
+    neopixelStrip.setPixelColor(0, neopixel.colors(NeoPixelColors.Red))
+    neopixelStrip.show()
+    basic.pause(2000)
+})
