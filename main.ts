19e91fe7e353be71bbcfb9452b35916a5623ddb5
Ali-Elouasbi <ali.elouasbi@mths.ca>
2024-10-15 10:55:13 -0400

    rgb

--- main.ts
+++ main.ts
@@ -9,23 +9,21 @@
 let neopixelStrip: neopixel.Strip = null
 
 basic.forever(function() {
-    // cleanup
+    // turns on Green light
     basic.clearScreen()
     neopixelStrip = neopixel.create(DigitalPin.P16, 3, NeoPixelMode.RGB)
     neopixelStrip.setPixelColor(2, neopixel.colors(NeoPixelColors.Green))
-    neopixelStrip.setPixelColor(1, neopixel.colors(NeoPixelColors.Black))
-    neopixelStrip.setPixelColor(0, neopixel.colors(NeoPixelColors.Black))
     neopixelStrip.show()
     basic.pause(2000)
+    
+    // turns on Yellow light
     neopixelStrip = neopixel.create(DigitalPin.P16, 3, NeoPixelMode.RGB)
-    neopixelStrip.setPixelColor(2, neopixel.colors(NeoPixelColors.Black))
     neopixelStrip.setPixelColor(1, neopixel.colors(NeoPixelColors.Yellow))
-    neopixelStrip.setPixelColor(0, neopixel.colors(NeoPixelColors.Black))
     neopixelStrip.show()
     basic.pause(1000)
+    
+    // turns on Red light
     neopixelStrip = neopixel.create(DigitalPin.P16, 3, NeoPixelMode.RGB)
-    neopixelStrip.setPixelColor(2, neopixel.colors(NeoPixelColors.Black))
-    neopixelStrip.setPixelColor(1, neopixel.colors(NeoPixelColors.Black))
     neopixelStrip.setPixelColor(0, neopixel.colors(NeoPixelColors.Red))
     neopixelStrip.show()
     basic.pause(2000)
